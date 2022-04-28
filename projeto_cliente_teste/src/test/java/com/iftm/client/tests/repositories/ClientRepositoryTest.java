672f081babc5fc51c4d3087bbafb4d2a4994dd49
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-04-28 20:41:55 -0300

    Criação dos cenários de teste 3 e 4.

--- projeto_cliente_teste/src/test/java/com/iftm/client/tests/repositories/ClientRepositoryTest.java
+++ projeto_cliente_teste/src/test/java/com/iftm/client/tests/repositories/ClientRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.iftm.client.tests.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.iftm.client.entities.Client;
 import com.iftm.client.repositories.ClientRepository;
@@ -30,9 +32,12 @@ public class ClientRepositoryTest {
 		 */
 		@Test
 		public void testarSeDeleteApagaClienteComIdExistente() {
+			//cenário
 			long id = 1;
 			repositorio.deleteById(id);
-			Assertions.assertFalse(repositorio.existsById(id));
+			Optional<Client> resposta = repositorio.findById(id);
+			Assertions.assertFalse(resposta.isPresent());
+			
 		}
 		
 		/**
@@ -49,10 +54,45 @@ public class ClientRepositoryTest {
 		@Test
 		public void testarSeDeleteRetornaExceptionCasoIdNaoExiste() {
 			long id = 10000;
-			Assertions.assertThrows(Exception.class, ()->{repositorio.deleteById(id);});
-			
-			
+			Assertions.assertThrows(EmptyResultDataAccessException.class, ()->{repositorio.deleteById(id);});
 		}
 
+		/**
+		  Cenário de teste 3.
+		  Objetivo: Verificar se a exclusão de todos elementos realmente apaga todos os registros do Banco de dados.
+			monta o cenário,
+				- arquivo import.sql carrega o cenário (clientes cadastrados)
+			executa a ação
+				- executar o método de exclusão de todos registros
+			e valida a saída.
+				- consultar todos os registros do banco e verificar se retorna vazio.
+		 */
+		@Test 
+		public void testarSeDeleteApagaTodosRegistros() {
+			repositorio.deleteAll();
+			List<Client> resultado = repositorio.findAll();			
+			Assertions.assertTrue(resultado.isEmpty());
+		}
 		
+		/**
+		 Cenário de teste 4
+		 Objetivo: Verificar se a exclusão de uma entidade existente no banco de dados realmente ocorre.
+			monta o cenário,
+				- arquivo import.sql carrega o cenário (clientes cadastrados)
+				- id de um cliente que existe em import.sql
+			executa a ação
+				- executa o método encontrar por id para retornar a entidade do	cliente com id informado.
+				- executa o método apagar a entidade
+				- executar novamente o método encontrar por id e verificar se o retorno dele é vazio
+			e valida a saída.
+				- verifica se o retorno do método encontrar por id é vazio.
+		 */
+		@Test
+		public void testarSeDeleteApagaUmRegistroComIDExistente() {
+			long id = 4;
+			Optional<Client> cliente = repositorio.findById(id);
+			repositorio.delete(cliente.get());
+			Optional<Client> resultado = repositorio.findById(id);
+			Assertions.assertTrue(resultado.isEmpty());
+		}
 }
